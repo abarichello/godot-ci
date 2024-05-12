3d5496ea47a4050977675441fabffa8f47dc42ae
Kilian Gärtner <Bhaals@gmx.de>
2024-05-12 10:42:31 +0200

    chore: clean up project

--- test-project/README.md
+++
@@ -1,3 +0,0 @@
-# test-project
-
-Example project used for [godot-ci](https://github.com/aBARICHELLO/godot-ci).<br>
--- test-project/scripts/Arena.gd
+++
@@ -1,7 +0,0 @@
-extends Node2D
-
-func _ready():
-	self.setup_arena()
-
-func setup_arena() -> void:
-	$Background.lowlight()
